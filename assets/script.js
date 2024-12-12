ac14e66e269fecba79180a8721626c639514fc89
JohnReiiss <johnatan.reiiss@icloud.com>
2024-12-11 22:17:37 -0300

    conversor dolar reaL SEM API version 0.1.0

---
+++ assets/script.js
@@ -0,0 +1,62 @@
+let dolar = 5.95;
+
+let usdInput = document.querySelector("#usd");
+let blrInput = document.querySelector("#brl");
+
+usdInput.addEventListener("keyup", () => {
+    convert("usd-to-brl")
+});
+
+blrInput.addEventListener("keyup", () => {
+    convert("brl-to-usd")
+});
+
+usdInput.addEventListener("blur", () => {
+    usdInput.value = formatCurrency(usdInput.value)
+})
+
+blrInput.addEventListener("blur", () => {
+    blrInput.value = formatCurrency(blrInput.value)
+})
+
+usdInput.value = "5,95";
+convert("usd-to-brl");
+
+// Funções
+function formatCurrency(value) {
+    let fixedValue = fixValue(value);
+    let options = {
+        useGrouping: false,
+        minimumFractionDigits: 2
+    };
+    let formatter = new Intl.NumberFormat("pt-BR", options);
+    return formatter.format(fixedValue);
+}
+
+function fixValue(value) {
+    let fixedValue = value.replace(",", ".");
+    let floatValue = parseFloat(fixedValue);
+    if (isNaN(floatValue)) {
+        floatValue = 0;
+    }
+    return floatValue;
+}
+
+function convert(type) {
+    if (type == "usd-to-brl") {
+        let fixedValue = fixValue(usdInput.value)
+
+        let result = fixedValue * dolar
+        result = result.toFixed(2)
+
+        blrInput.value = formatCurrency(result)
+    }
+    if (type == "brl-to-usd") {
+        let fixedValue = fixValue(blrInput.value)
+
+        let result = fixedValue / dolar
+        result = result.toFixed(2)
+
+        usdInput.value = formatCurrency(result)
+    }
+}
